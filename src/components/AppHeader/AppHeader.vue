d55bff2580abdb3d1dba069baa3bc3c40ff26096
Antek93 <antonio.savino1993@gmail.com>
2023-02-06 16:29:04 +0100

    creato header

--- src/components/AppHeader/AppHeader.vue
+++ src/components/AppHeader/AppHeader.vue
@@ -12,9 +12,21 @@ export default {
 </script>
 
 <template>
-  
+    <header>
+        <div>
+            <!-- Logo Yu-gi-ho  -->
+            <div class="logo ps-4">
+              <img class="img-fluid" src="http://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es" alt="">
+             </div>
+
+             <!-- Scritta Yu-gi-ho  -->
+             <div class="container-fluid">
+                 Yu-Gi-ho Api
+             </div>
+        </div>
+    </header>
 </template>
 
 <style lang="scss">
-
+@import '../../styles/partials/HeaderAttributes/HeaderAttributes.scss';
 </style>
