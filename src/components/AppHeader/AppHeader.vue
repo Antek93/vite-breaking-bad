6b970ef052955f342317c718d69c98ebd4174a82
Antek93 <antonio.savino1993@gmail.com>
2023-02-06 16:08:48 +0100

    aggiunto bootstrap

---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data () {
+        return {
+            msg: 'Ciao',
+            count: 0,
+        }
+
+    }
+};
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+
+</style>
